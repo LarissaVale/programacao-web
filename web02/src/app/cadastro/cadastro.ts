22a7924c029a906a5b674b2375e357f54eb85496
larissa vale <larissavitorianogueira13@gmail.com>
2026-03-13 14:51:29 -0300

    cria model cliente e salva objeto no localStorage

--- web02/src/app/cadastro/cadastro.ts
+++ web02/src/app/cadastro/cadastro.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../model/cliente';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
 export class Cadastro {
-  public aviso:String = "";
+  public mensagem:String = "";
+  public cliente: Cliente = new Cliente();
 
   public cadastrar(){
-    this.aviso = "Seu cadastro foi realizado com sucesso!";
+    let json = JSON.stringify(this.cliente);
+    localStorage.setItem("clienteCadastro", json)
+    this.mensagem = "Seu cadastro foi realizado com sucesso!";
   }
 }
---
+++ web02/src/app/model/cliente.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+  nome: String = "";
+  cpf: String = "";
+  dataNascimento: String = "";
+  telefone: String = "";
+  cep: String = "";
+  logradouro: String = "";
+  email: String = "";
+}
